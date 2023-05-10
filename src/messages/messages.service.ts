eee42b9db678718ab8baa4925d37b0b899a92998
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-10 12:53:59 +0200

    feat(service): implrment service

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from "./messages.repository";
+
+export class MessagesService {
+  
+  messagesRepo: MessagesRepository;
+  
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  async findOne(id: string){
+    return this.messagesRepo.findOne(id);
+  }
+
+  async findAll(){
+    return this.messagesRepo.findAll();
+  }
+
+  async create(content: string){
+    return this.messagesRepo.create(content);
+  }
+}
