facb9a6c61b25e72b513e0fedad8fc99423cf0dc
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-10 12:58:16 +0200

    feat(app): use service in controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,27 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  
+  messagesService: MessagesService;
+
+  constructor(){
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body)
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id)
+    return this.messagesService.findOne(id);
   }
 }
