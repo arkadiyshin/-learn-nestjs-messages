9c9e3b5dbc47dd5ed86d6c4c1ff5e04d60b51ee5
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-10 11:54:35 +0200

    feat(controller): add routing logic

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessage() {}
+
+  @Get('/:id')
+  getMessage() {}
+}
